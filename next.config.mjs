679b14f73443058df210b2b3a30a1ffe13b4b2d3
Muhammad Chamdani Sukron <scrum@yatimmandiri.org>
2024-02-21 10:28:41 +0700

    Initialize
    Initialize Project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+  images: {
+    dangerouslyAllowSVG: true,
+  },
+};
 
 export default nextConfig;
